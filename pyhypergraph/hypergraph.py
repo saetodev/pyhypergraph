bce715fc313e9e401ac05d429bd971ee660f6473
Ndenisson <alanndenisson@gmail.com>
2026-01-01 15:30:58 -0500

    Added two new properties to the hypergraph. \n Added the methods order and size with method order returning the number of vertices and method size returning the number of edges. \n Also added a line in the test python file to test the two new methods

--- pyhypergraph/hypergraph.py
+++ pyhypergraph/hypergraph.py
@@ -1,6 +1,6 @@
 
 class Hypergraph:
-    def __init__(self, vertices, edges):
+    def __init__(self, vertices: list, edges: list):
         self._vertices = vertices
         self._edges = edges
 
@@ -11,6 +11,16 @@ class Hypergraph:
     @property
     def edges(self):
         return self._edges
+    
+    #The following two methods will return the order and cardinality of the hypergraph
+
+    @property
+    def order(self):
+        return len(self._vertices)
+    
+    @property
+    def size(self):
+        return len(self._edges)
 
 def reduce_hypergraph(h: Hypergraph) -> Hypergraph:
     edges_reduced = h.edges.copy()
--- tests/test_hypergraph.py
+++ tests/test_hypergraph.py
@@ -7,3 +7,6 @@ def test_reduce_hypergraph():
 
     assert h_reduced.vertices == h.vertices
     assert h_reduced.edges == [{0, 1, 2}, {0, 2, 3}]
+
+h = Hypergraph([0,1,2,3,4],[[0,1,2],[0,1],[0,2,3],[2,3]])
+print(f"Vertices: {h.vertices} \n Edges: {h.edges} \n Order: {h.order} \n Size: {h.size}")
