fc4bec9c7309ac1d4892d2b0b2e8baf00b3f490d
Ndenisson <alanndenisson@gmail.com>
2026-01-03 22:11:14 -0500

    added one method (the is_trivial method) and one function (coloring function)
    the is_trivial method just determines whether or not a hypergraph is trivial.
    the coloring function colors a hypergraph such that no edges is monochromatic.
    
    details about both function can be found in their docstring

--- pyhypergraph/hypergraph.py
+++ pyhypergraph/hypergraph.py
@@ -1,3 +1,4 @@
+import random
 
 class Hypergraph:
     def __init__(self, vertices: list = [], edges: list = []):
@@ -31,7 +32,7 @@ class Hypergraph:
         """
         Docstring for is_empty
         
-        (any) -> bool
+        () -> bool
         determine if a hypergraph is empty or not i.e. if the hypergraph has no edges nor vertices.
 
         Example
@@ -43,6 +44,24 @@ class Hypergraph:
         """
 
         return not self.vertices and not self.edges
+    
+    #Determines if a hypergraph is trivial
+    def is_trivial(self):
+        """
+        Docstring for is_trivial
+        
+        () -> bool
+        determine if a hypergraph is trivial i.e. if the hypergraph has no edges but has vertices.
+
+        Example
+        h = Hypergraph([1],[])
+        print(h.is_trivial())
+
+        >>> True
+
+        """
+
+        return not not self.vertices and not self.edges
 
 def reduce_hypergraph(h: Hypergraph) -> Hypergraph:
     edges_reduced = h.edges.copy()
@@ -84,3 +103,64 @@ def star_center_at(h:Hypergraph, vertex) -> list:
             incident_edges.append(e)
 
     return incident_edges
+
+#Hypergraph greedy coloring algorithm
+def coloring(h: Hypergraph) -> list:
+
+    """
+    Docstring for coloring
+    
+    (Hypergraph) -> list
+
+    This function takes in a hypergraph as parameter and color the vertices of the graph such that no edge (that is not
+    a loop) contains only one color (i.e is monochromatic).
+
+    The algorithm works by taking the vertices, and for each vertex, we determine to which coloring partitions it can be added
+    without an edge been monochromatic; then we take the first coloring partition that fits and we insert the vertex there.
+
+    To determine which partition is not valid for a given vertex, we simply check if by removing the vertex from its incident edges, 
+    the edges become monochromatic. If it becomes monochromatic we know that the given color partition is not a valid and we can exclude
+    it from the list of given partition.
+
+    if no valid partition is found after verifying which coloring partition can fit the vertex, we just create a new one with the vertex
+    as first entry. 
+
+    Note: The algorithm used is greedy as such it may not always return the optimal solution if the other is not correct.
+    - To give the algorithm a chance to return the optimal solution, the vertices are shuffled.
+    - Also when refering to color_partition, I mean a list of vertices that are colored the same.
+    """
+
+    coloring_partition = []
+
+    if h.is_empty():
+        return coloring_partition
+    
+    coloring_partition.append([])
+
+    #shuffle the vertices for the greedy algorithm
+    shuffled_vertices = h.vertices
+    random.shuffle(shuffled_vertices)
+
+    for  v in shuffled_vertices:
+    
+        forbidden_partition = [] #list of partition that cannot take v without an edge been monochromatics
+
+        for e in star_center_at(h,v):
+
+            e_without_v = [ x for x in e if x != v ] #removing v from its incident esges
+
+            if not e_without_v: #skip if the edge is a loop
+                continue
+
+            for c in coloring_partition:
+                if set(e_without_v).issubset(set(c)): #verify if an edge is almost monochromatic (i.e lacks v to be colored the same as the other vertices to become monochromatic)
+                    forbidden_partition.append(c) #adds the partition as invalid partition for vertex v
+
+        available_partition = [x for x in coloring_partition if x not in forbidden_partition] #finds all the partition that can take v without any edge be monochromatic
+
+        if not available_partition: #create a new partition if v can't be fitted in any partition without an edge been monochromatic
+            coloring_partition.append([v])
+        else: #adds v to the first partition where it fits.
+            coloring_partition[coloring_partition.index(available_partition[0])].append(v)
+
+    return coloring_partition
--- tests/test_hypergraph.py
+++ tests/test_hypergraph.py
@@ -1,4 +1,4 @@
-from pyhypergraph import *
+from pyhypergraph.hypergraph import *
 
 def test_reduce_hypergraph():
     h = Hypergraph([0, 1, 2, 3], [{0, 1, 2}, {0, 1}, {0, 2, 3}, {2, 3}])
@@ -8,5 +8,13 @@ def test_reduce_hypergraph():
     assert h_reduced.vertices == h.vertices
     assert h_reduced.edges == [{0, 1, 2}, {0, 2, 3}]
 
-h = Hypergraph([0,1,2,3,4],[[0,1,2],[0,1],[0,2,3],[2,3]])
-print(f"Vertices: {h.vertices} \n Edges: {h.edges} \n Order: {h.order} \n Size: {h.size}")
+h = Hypergraph([1,2,3,4,5],[[1,2,3],[1,2,5],[3,4]])
+x = Hypergraph()
+#print(f"Vertices: {h.vertices} \n Edges: {h.edges} \n Order: {h.order} \n Size: {h.size}")
+#print(h.is_empty())
+#print(x.is_empty())
+#print(x.is_trivial())
+#
+#print(star_center_at(h,2))
+
+print(coloring(h))
